c6aeeffe92de331a7061c2ae1ae9a1287ba76839
0zzycodes <horlalekan123@gmail.com>
2020-05-21 15:51:51 +0800

    wrote a function that fillter out expired trips

--- src/pages/alltrip/alltrip.jsx
+++ src/pages/alltrip/alltrip.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
 import { selectAllTrip } from '../../redux/trip/trip.selectors';
@@ -7,20 +7,35 @@ import TripPreview from '../../components/trip-preview/trip-preview';
 import './alltrip.scss';
 
 const AllTripPage = ({ trips }) => {
+  const [state, setState] = useState({ treeps: [] });
+  useEffect(() => {
+    const treeps = [];
+    trips.forEach((item) => {
+      let end = new Date(`${item.date} ${item.time}`);
+      let now = new Date();
+      let distance = end - now;
+      if (distance <= 0) {
+        item.isExpired = true
+      }
+      treeps.push(item)
+    });
+
+    return setState({ treeps });
+  }, [trips]);
   return (
     <div className="all-trip-page">
       <div className="container">
         <h1>
-          ({trips.length}
+          ({state.treeps.length}
           )Trips
         </h1>
-        {trips.length !== 0
+        {state.treeps.length !== 0
           ? trips
-              .filter((item, index) => index < 10)
-              .map((trip, index) => <TripPreview key={index} trip={trip} />)
+            .filter((item, index) => index < 10)
+            .map((trip, index) => trip.isExpired ? <TripPreview key={index} trip={trip} expired /> : <TripPreview key={index} trip={trip} />)
           : null}
         <div className="all-trip">
-          {trips.length > 10 ? (
+          {state.treeps.length > 10 ? (
             <button className="btn">Load More</button>
           ) : null}
         </div>
